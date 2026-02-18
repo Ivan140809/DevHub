9984e9272030ef353d1d2ba46411c655b1ff1c52
Lorenzo Ramirez <lorenzoramirez.lrc@gmail.com>
2026-02-17 22:31:32 -0500

    Configured Java Dependencies Spring Boot and Mongo

---
+++ src/main/java/com/skillstack/devhub/SrcApplication.java
@@ -0,0 +1,13 @@
+package com.skillstack.devhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrcApplication.class, args);
+    }
+
+}
