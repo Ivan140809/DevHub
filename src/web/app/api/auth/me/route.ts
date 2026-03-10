dc1c9b6c1bea129d7aa1ee2e12a4f7b2e5932835
Ivan140809 <ivansantiagolastra1408@gmail.com>
2026-03-10 08:53:26 -0500

    fix:correction of cookies

--- src/web/app/api/auth/me/route.ts
+++ src/web/app/api/auth/me/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import {cookies } from 'next/headers';
 
 export async function GET() {
-    const token = cookies().get("token")?.value;
+    const token = (await cookies()).get("token")?.value;
     if (!token) {
         return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
