d96794a4627630d0356fb07e174fba364c0c3106
Richard Castillo <richard.castillo.1236@gmail.com>
2026-03-13 19:56:41 -0500

    feat: Metodo getQuestions, paginacion de 10 preguntas (#126)

--- src/main/java/com/skillstack/devhub/service/QuestionService.java
+++ src/main/java/com/skillstack/devhub/service/QuestionService.java
@@ -6,6 +6,9 @@ import com.skillstack.devhub.model.Option;
 import com.skillstack.devhub.model.Question;
 import com.skillstack.devhub.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -39,5 +42,11 @@ public class QuestionService {
         Question q = new Question(question.getTitulo(), question.getEnunciado(), question.getCategoria(), question.getDificultad(), options);
 
         questionRepository.save(q);
+    }
+
+
+    public Page<Question> getQuestions(int page){
+        Pageable pageable = PageRequest.of(page, 10);
+        return questionRepository.findAll(pageable);
     }
 }
