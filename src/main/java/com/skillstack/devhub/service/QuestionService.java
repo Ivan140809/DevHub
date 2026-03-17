ec6a1104762254d13385398f1125b1e5f91a721b
Richard Castillo <richard.castillo.1236@gmail.com>
2026-03-17 14:46:25 -0500

    feat: Agregar método para filtrar por dificultad (#150)

--- src/main/java/com/skillstack/devhub/service/QuestionService.java
+++ src/main/java/com/skillstack/devhub/service/QuestionService.java
@@ -3,6 +3,7 @@ package com.skillstack.devhub.service;
 import com.skillstack.devhub.dto.OptionDTO;
 import com.skillstack.devhub.dto.QuestionDTO;
 import com.skillstack.devhub.model.Category;
+import com.skillstack.devhub.model.Dificultad;
 import com.skillstack.devhub.model.Option;
 import com.skillstack.devhub.model.Question;
 import com.skillstack.devhub.repository.QuestionRepository;
@@ -72,5 +73,20 @@ public class QuestionService {
                 q.getDificultad(),
                 null
         )).toList();
+    }
+
+    public List<QuestionDTO> getQuestionByDifficulty (Dificultad difficulty, int page){
+        Pageable pageable = PageRequest.of(page, 10);
+        Page<Question> questionPage = questionRepository.findByDifficulty(difficulty, pageable);
+
+        return questionPage.getContent().stream().map(q ->
+                new QuestionDTO(
+                        q.getTitle(),
+                        null,
+                        q.getCategoria(),
+                        q.getDificultad(),
+                        null
+                )
+        ).toList();
     }
 }
