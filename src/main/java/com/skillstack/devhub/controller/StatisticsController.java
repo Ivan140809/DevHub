9a87c9f4735ee19c5f28697d93e24d2c8b9043e2
brewLux <luxfusan@gmail.com>
2026-04-02 23:51:51 -0500

    feat: crear StatisticsController y pequeno fix de corregir metodo createReview #241

---
+++ src/main/java/com/skillstack/devhub/controller/StatisticsController.java
@@ -0,0 +1,22 @@
+package com.skillstack.devhub.controller;
+
+import com.skillstack.devhub.service.StatisticsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/statistics")
+@CrossOrigin
+public class StatisticsController {
+
+    private final StatisticsService statisticsService;
+
+    @Autowired
+    public StatisticsController(StatisticsService statisticsService) {
+        this.statisticsService = statisticsService;
+    }
+
+}
