ffcf7e77f4ba9ee3768bf6230481583236fadce9
Richard Castillo <richard.castillo.1236@gmail.com>
2026-04-09 19:50:36 -0500

    feat: FactoryMethod para creación de Users #276

--- src/main/java/com/skillstack/devhub/model/User.java
+++ src/main/java/com/skillstack/devhub/model/User.java
@@ -1,105 +1,19 @@
 package com.skillstack.devhub.model;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.util.List;
 
 
 @Document(collection = "users")
-public class User {
-
-    @Id
-    private String id;
-    private String firstName;
-    private String lastName;
-    private String username;
-    private String email;
-    private String password;
-    private String phone;
-    private List<String> preferences;
-    private Role role;
-    private String totalScore;
-
-    public User(){}
-
-    public User(String firstName, String lastName, String username, String email,
-                String password, Role role, String totalScore) {
+public class User extends AbstractUser{
+    public User(String firstName, String lastName, String username, String email, String password, String phone, Role role, int totalScore) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.username = username;
         this.email = email;
         this.password = password;
+        this.phone = phone;
         this.role = role;
         this.totalScore = totalScore;
     }
 
-    public String getId(){
-        return id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) { this.password = password; }
-
-    public String getPhone() { return phone; }
-    public void setPhone(String phone) { this.phone = phone; }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public List<String> getPreferences() {
-        return preferences;
-    }
-
-    public void setPreferences(List<String> preferences) {
-        this.preferences = preferences;
-    }
-
-    public String getTotalScore(){
-        return totalScore;
-    }
-
-    public void setTotalScore(String totalScore){
-        this.totalScore = totalScore;
-    }
 }
