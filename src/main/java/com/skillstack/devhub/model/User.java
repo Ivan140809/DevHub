7575db35e4f3c642838589e32de8bcb7a8141fe4
Richard Castillo <richard.castillo.1236@gmail.com>
2026-04-14 12:21:29 -0500

    fix: Cambio factory clase abstracta

--- src/main/java/com/skillstack/devhub/model/User.java
+++ src/main/java/com/skillstack/devhub/model/User.java
@@ -6,6 +6,9 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
 public class User extends AbstractUser{
+    @Id
+    private String id;
+
     public User(String firstName, String lastName, String username, String email, String password, String phone, Role role, int totalScore) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -16,6 +19,10 @@ public class User extends AbstractUser{
         this.role = role;
         this.phone = phone;
         this.totalScore = totalScore;
+    }
+
+    public String getId() {
+        return id;
     }
 
 }
