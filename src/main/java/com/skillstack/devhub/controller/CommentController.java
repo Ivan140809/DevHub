44466ccfaa7c68a37fe89e4716b1b3bad9350f0f
brewLux <luxfusan@gmail.com>
2026-04-18 21:03:57 -0500

    feat: crear endpoint para ver comentarios dentro de CommentController #342

--- src/main/java/com/skillstack/devhub/controller/CommentController.java
+++ src/main/java/com/skillstack/devhub/controller/CommentController.java
@@ -1,7 +1,10 @@
 package com.skillstack.devhub.controller;
 
+import com.skillstack.devhub.dto.CommentDTO;
 import com.skillstack.devhub.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,6 +17,16 @@ public class CommentController {
     @Autowired
     public CommentController(CommentService commentService) {
         this.commentService = commentService;
+    }
+
+    @GetMapping("/{commentId:[0-9a-f]{24}}")
+    public ResponseEntity<CommentDTO> getComment(
+            @PathVariable String questionId,
+            @PathVariable String commentId) {
+        CommentDTO comment = commentService.getCommentTree(commentId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(comment);
     }
 
 }
